f727852d0da45039b78c4479fcd7bf05d634c695
TheWozard <dan620@gmail.com>
2025-10-19 10:47:59 -0400

    feat: able to configure device color

--- src/lib.rs
+++ src/lib.rs
@@ -9,46 +9,62 @@ pub fn open_first_device() -> Result<PanelDevice, HidError> {
             (0x0483, 0xa3c4) // PC Panel Mini
             => {
                 let device = device_info.open_device(&api)?;
-                return Ok(PanelDevice { device, handler: PanelHandler::new(4) });
+                return Ok(PanelDevice { 
+                    device, 
+                    device_id: 0x06,
+                    color_set_id: 0x05,
+                    handler: PanelHandler::new(4) 
+                });
             }
             _ => continue,
         }
     }
     Err(Error::new(
         ErrorKind::NotFound,
-        "No compatible HID device found",
+        "no compatible HID device found",
     ).into())
 }
 
 pub struct PanelDevice {
     device: HidDevice,
+    device_id: u8,
+    color_set_id: u8,
+
     pub handler: PanelHandler,
 }
 
 impl PanelDevice {
-    pub fn open_stream(self) {
+    const SET_FULL_COLOR: u8 = 0x04;
+
+    pub fn open_stream(self) -> Result<(), HidError> {
         loop {
-            let mut buf = [0u8; 3];
+            let mut buf = [0u8; 64];
             match self.device.read(&mut buf) {
-                Ok(bytes_read) if bytes_read > 0 => {
-                    match buf.get(0) {
-                        Some(&0x01) => self.handler.rotate(
-                            buf.get(1).cloned().unwrap_or(0) as usize,
-                            buf.get(2).cloned().unwrap_or(0),
-                        ),
-                        Some(&0x02) => self.handler.click(
-                            buf.get(1).cloned().unwrap_or(0) as usize,
-                        ),
+                Ok(bytes_read) if bytes_read >= 3 => {
+                    match buf[0] {
+                        0x01 => self.handler.rotate(buf[1] as usize, buf[2]),
+                        0x02 => self.handler.click(buf[1] as usize),
                         _ => {},
                     }
                 }
                 Ok(_) => continue, // No data read
                 Err(e) => {
-                    eprintln!("\nError reading from device: {}", e);
-                    break;
+                    return Err(Error::new(
+                        ErrorKind::Other,
+                        format!("error reading from device: {}", e),
+                    ).into());
                 }
             }
         }
+    }
+
+    pub fn set_color(&self) -> Result<(), Error> {
+        self.device.write(&[
+            self.device_id, PanelDevice::SET_FULL_COLOR, self.color_set_id, 
+            0x00, 0xff, 0xff
+        ]).map(|_| ()).map_err(|result| {
+            Error::new(ErrorKind::Other, format!("failed to set color: {}", result))
+        })
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,12 @@ fn main() {
             device.handler.register_rotate_callback(0, |amount| {
                 println!("Button 0 rotated by {}", amount);
             });
-            device.open_stream();
+            if let Err(e) = device.set_color() {
+                eprintln!("Failed to set color: {}", e);
+            }
+            if let Err(e) = device.open_stream() {
+                eprintln!("Error during device stream: {}", e);
+            }
         }
         Err(e) => {
             eprintln!("Failed to open device: {}", e);
